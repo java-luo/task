486afb1e29f4e8e6f0c778402ac8148906c0ac4d
L <1551528143@qq.com>
2020-07-11 14:54:45 +0800

    修复了redis链接不上的BUG

--- com/itcast/utils/iniUtil.py
+++ com/itcast/utils/iniUtil.py
@@ -3,10 +3,13 @@ from com.itcast.utils.PathUtil import *
 rootPath=getRootPath()
 config = configparser.ConfigParser()
 # -read读取ini文件
-config.read(rootPath+'./config.ini', encoding='GB18030')
+print(rootPath)
+config.read('./config.ini')
 
 
 # 输入分组和key值 返回value
 def getInI(section, option):
     return config.get(section, option)
 
+
+print(getInI("spider", "get_ticket_num_url"))
